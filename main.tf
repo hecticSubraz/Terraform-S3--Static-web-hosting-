d088f6cee5920c75bddeaef0c17003e0e4e80625
Subraz Thapa <subrazthapa33@gmail.com>
2025-06-02 06:30:09 +0000

    Initial commit - Terraform S3 static site

---
+++ main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket        = var.bucket_name
+
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.static_site.id
+ 
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "index.html"
+  source       = var.index_file
+  content_type = "text/html"
+    etag         = filemd5("index.html")
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "error.html"
+  source       = var.error_file
+  content_type = "text/html"
+    etag         = filemd5("error.html")
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "bucket_name" {
+  default = "subrazbuck-07"
+}
+
+variable "index_file" {
+  default = "index.html"
+}
+
+variable "error_file" {
+  default = "error.html"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
